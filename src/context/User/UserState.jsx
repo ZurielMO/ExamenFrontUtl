2419c12f197bf7393c36475b7800a29c87b3da6c
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-11-07 17:05:27 -0600

    minuscula de boostrap

--- src/context/User/UserState.jsx
+++ src/context/User/UserState.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 import UserContext from './UserContext';
 import { POST_USER } from '../types';
 import {Alert, Breadcrumb, Button, Form, Row,Col, Container} from 'react-bootstrap'
-import 'Bootstrap/dist/css/Bootstrap.min.css'
+import 'bootstrap/dist/css/Bootstrap.min.css'
 
 
 const UserState = (props) => {
